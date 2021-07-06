a83bb290eef28854ca85b3fd0f0148274909a81d
blue_islands <h.aoshima@livlog.jp>
2021-07-07 07:46:26 +0900

    no message

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,4 +1,5 @@
 import org.goldrenard.jb.configuration.BotConfiguration;
+import org.goldrenard.jb.configuration.LanguageConfiguration;
 import org.goldrenard.jb.core.Bot;
 import org.goldrenard.jb.core.Chat;
 
@@ -6,12 +7,28 @@ public class Test {
 
     public static void main(String[] args) {
 
-//        LanguageConfiguration lang = new LanguageConfiguration(null, null, null, null, null, null, null);
+        final var defaultResponse = "私にはその答えがありません。";
+        final var errorResponse = "私の脳に何か問題があります。";
+        final var scheduleError = "そのイベントをスケジュールできません。";
+        final var systemFailed = "システムコマンドの実行に失敗しました。";
+        final var templateFailed = "テンプレートが失敗しました。";
+        final var tooMuchRecursion = "AIMLの再帰が多すぎます。";
+        final var tooMuchLooping = "AIMLのループが多すぎます。";
+
+        final var lang = LanguageConfiguration.builder();
+        lang.defaultResponse(defaultResponse);
+        lang.errorResponse(errorResponse);
+        lang.scheduleError(scheduleError);
+        lang.systemFailed(systemFailed);
+        lang.templateFailed(templateFailed);
+        lang.tooMuchRecursion(tooMuchRecursion);
+        lang.tooMuchLooping(tooMuchLooping);
 
         final var bot = new Bot(BotConfiguration.builder()
                 .name("alice")
                 .path("src/main/resources")
                 .jpTokenize(true)
+                .language(lang.build())
                 .build());
 
         final var chatSession = new Chat(bot);
